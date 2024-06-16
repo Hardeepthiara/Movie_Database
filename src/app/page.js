205cd086a7fd5d2300c45fb5745b1f7c38f1cb30
Hardeep <hardeepkaur.bca@gmail.com>
2024-06-16 16:59:01 -0400

    StreamSearch

--- src/app/page.js
+++ src/app/page.js
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
             className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary underline-offset-4 hover:underline h-10 w-10"
-            href=""
+            href="https://github.com/Hardeepthiara/Movie_Database"
           >
             <svg
               stroke="currentColor"
